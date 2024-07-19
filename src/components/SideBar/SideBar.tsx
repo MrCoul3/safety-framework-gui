74086d84b077d1a82f96c10d4335651e44081a31
IvanovNV <inv@algont.ru>
2024-07-19 08:21:42 +0300

    fix opening dashboard;

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,29 +1,32 @@
-import React, {useState} from "react";
+import React from "react";
 import { observer } from "mobx-react-lite";
 import style from "./style.module.css";
-import { useTranslation } from "react-i18next";
-import { SubGroupsActionsTypes, SubGroupsTypes } from "enums/SubGroupsTypes";
+import { SubGroupsActionsTypes } from "enums/SubGroupsTypes";
 import SideBarSubGroup from "../SideBarSubGroup/SideBarSubGroup";
-import { IconBento } from "@consta/icons/IconBento";
-import { IconList } from "@consta/icons/IconList";
-import { IconMail } from "@consta/icons/IconMail";
-import { IconTrash } from "@consta/icons/IconTrash";
-import {IAction} from "../../interfaces/IAction";
-import {ISubGroupState} from "../../interfaces/ISubGroupState";
+import { IAction } from "../../interfaces/IAction";
+import { ISubGroupState } from "../../interfaces/ISubGroupState";
+import { DASHBOARD_URL } from "../../constants/constants";
 
 interface ISideBar {
   onItemClick(item: IAction): void;
 
-  subGroupsState: ISubGroupState[]
+  subGroupsState: ISubGroupState[];
 }
 
 const SideBar = observer((props: ISideBar) => {
+  const onItemClick = (item: IAction) => {
+    if (item.label === SubGroupsActionsTypes.DashBoard) {
+      window.open(DASHBOARD_URL, "_blank")?.focus();
+      return;
+    }
+    props.onItemClick(item);
+  };
   return (
     <div className={style.SideBar}>
       {props.subGroupsState.map((group: ISubGroupState) => (
         <SideBarSubGroup
           key={group.name}
-          onItemClick={props.onItemClick}
+          onItemClick={onItemClick}
           groupTitle={group.name}
           actions={group.actions}
         />
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {observer} from "mobx-react-lite";
+import {DASHBOARD_URL} from "../constants/constants";
 
 const DashboardPage = observer(() => {
-    const url = 'https://qv/QvAJAXZfc/opendoc.htm?document=%D0%BA%D0%B0%D1%80%D0%BA%D0%B0%D1%81%20%D0%B1%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D0%B8%5C%D0%BA%D0%B0%D1%80%D0%BA%D0%B0%D1%81%20%D0%B1%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D0%B8.qvw&lang=ru-RU&host=QVS%40GPN-PROD'
+    const url = DASHBOARD_URL;
     return (
         <div>
             <iframe style={{width: '100%', height: '100%'}} src={url}></iframe>
